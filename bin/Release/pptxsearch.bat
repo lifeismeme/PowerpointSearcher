44c735b5312f110b90e7eea325c0ea449a424562
lifeismeme <59262137+lifeismeme@users.noreply.github.com>
2020-08-22 00:22:58 +0800

    update .bat to work correctly

--- bin/Release/pptxsearch.bat
+++ bin/Release/pptxsearch.bat
@@ -1,2 +1,2 @@
 @echo off
-dir /w | findstr "\.pptx$" | dotnet "%~dp0./netcoreapp2.1/PowerpointSearcher/PowerpointSearcher.dll" %1
+dir *.pptx /b | findstr "\.pptx$" | dotnet "%~dp0./netcoreapp2.1/PowerpointSearcher/PowerpointSearcher.dll" %1
